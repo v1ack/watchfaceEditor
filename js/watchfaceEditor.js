52fbb052210611819ded43a05a1f85eaec8129e0
Vlack <kirilkin12@gmail.com>
2018-04-14 11:54:46 +0300

    New theme

--- js/watchfaceEditor.js
+++ js/watchfaceEditor.js
@@ -49,30 +49,59 @@ var wfe = {
                 });
             }
         },
-        changeTheme: function () {
-            if (document.body.classList.contains('uk-light')) {
-                localStorage.appTheme = 'light';
-                document.body.classList.remove('uk-light');
-                document.getElementsByTagName('html')[0].classList.remove('uk-background-secondary');
-                $('vars').classList.remove('uk-card-secondary');
-                $('modal-howto').childNodes[1].classList.remove('uk-background-secondary');
-                $('modal-about').childNodes[1].classList.remove('uk-background-secondary');
-                $('modal-donate').childNodes[1].classList.remove('uk-background-secondary');
-                $('jsonerrormodal').childNodes[1].classList.remove('uk-background-secondary');
-                $('modal-preview').childNodes[1].classList.remove('uk-background-secondary');
-                $('modal-settings').childNodes[1].classList.remove('uk-background-secondary');
-            } else {
-                localStorage.appTheme = 'dark';
-                document.body.classList.add('uk-light');
-                document.getElementsByTagName('html')[0].classList.add('uk-background-secondary');
-                $('vars').classList.add('uk-card-secondary');
-                $('modal-howto').childNodes[1].classList.add('uk-background-secondary');
-                $('modal-about').childNodes[1].classList.add('uk-background-secondary');
-                $('modal-donate').childNodes[1].classList.add('uk-background-secondary');
-                $('jsonerrormodal').childNodes[1].classList.add('uk-background-secondary');
-                $('modal-preview').childNodes[1].classList.add('uk-background-secondary');
-                $('modal-settings').childNodes[1].classList.add('uk-background-secondary');
+        changeTheme: function (theme) {
+            if (localStorage.appTheme == 'amazfit') {
+                document.getElementsByClassName('uk-navbar-left')[0].innerHTML = '<a class="uk-navbar-item uk-logo we-white" href="https://v1ack.github.io/watchfaceEditor/"><img src="assets/icon/android-chrome-192x192.png" style="width: auto; height: 60%; margin-right: 10px;">Watchface Editor</a>';
+                document.getElementsByTagName('html')[0].style.background = '';
+                document.getElementsByClassName('uk-navbar')[0].style.height = '';
+                document.getElementsByClassName('uk-navbar')[0].classList.add('we-white');
+                document.getElementsByClassName('uk-navbar')[0].classList.remove('amazfit');
+                document.getElementsByClassName('uk-navbar-container')[0].style.background = 'linear-gradient(to left, #28a5f5, #1e87f0)';
+                $('tablist').style.marginLeft = '15px';
             }
+            switch (theme) {
+                case 'light':
+                    localStorage.appTheme = 'light';
+                    document.body.classList.remove('uk-light');
+                    document.getElementsByTagName('html')[0].classList.remove('uk-background-secondary');
+                    $('vars').classList.remove('uk-card-secondary');
+                    $('modal-howto').childNodes[1].classList.remove('uk-background-secondary');
+                    $('modal-about').childNodes[1].classList.remove('uk-background-secondary');
+                    $('modal-donate').childNodes[1].classList.remove('uk-background-secondary');
+                    $('jsonerrormodal').childNodes[1].classList.remove('uk-background-secondary');
+                    $('modal-preview').childNodes[1].classList.remove('uk-background-secondary');
+                    $('modal-settings').childNodes[1].classList.remove('uk-background-secondary');
+                    break;
+                case 'dark':
+                    localStorage.appTheme = 'dark';
+                    document.body.classList.add('uk-light');
+                    document.getElementsByTagName('html')[0].classList.add('uk-background-secondary');
+                    $('vars').classList.add('uk-card-secondary');
+                    $('modal-howto').childNodes[1].classList.add('uk-background-secondary');
+                    $('modal-about').childNodes[1].classList.add('uk-background-secondary');
+                    $('modal-donate').childNodes[1].classList.add('uk-background-secondary');
+                    $('jsonerrormodal').childNodes[1].classList.add('uk-background-secondary');
+                    $('modal-preview').childNodes[1].classList.add('uk-background-secondary');
+                    $('modal-settings').childNodes[1].classList.add('uk-background-secondary');
+                    break;
+                case 'amazfit':
+                    wfe.app.changeTheme('dark');
+                    document.getElementsByClassName('uk-navbar-left')[0].innerHTML = '<a class="uk-navbar-item uk-logo we-white" href="https://amazfitwatchfaces.com/"><img src="assets/logo.png" style="width: 200px; image-rendering: auto;"></a>';
+                    localStorage.appTheme = 'amazfit';
+                    document.getElementsByTagName('html')[0].style.background = '#121314';
+                    document.getElementsByClassName('uk-navbar')[0].style.height = '50px';
+                    document.getElementsByClassName('uk-navbar')[0].classList.remove('we-white');
+                    document.getElementsByClassName('uk-navbar')[0].classList.add('amazfit');
+                    document.getElementsByClassName('uk-navbar-container')[0].style.background = '#222';
+                    $('tablist').style.marginLeft = '';
+                    break;
+            }
+        },
+        changeThemeBtn: function () {
+            if (document.body.classList.contains('uk-light'))
+                wfe.app.changeTheme('light');
+            else
+                wfe.app.changeTheme('dark');
         },
         lastimage: 300,
         imagestabversion: 2,
@@ -356,8 +385,7 @@ var wfe = {
             e.type = "text/javascript";
             document.getElementsByTagName("head")[0].appendChild(e);
         }
-        if (localStorage.appTheme == 'dark')
-            wfe.app.changeTheme();
+        wfe.app.changeTheme(localStorage.appTheme);
         if (!('lang' in localStorage))
             localStorage.lang = navigator.language || navigator.userLanguage;
         if (localStorage.lang.indexOf("ru") >= 0) {
@@ -2270,7 +2298,7 @@ var wfe = {
                     wfe.analog.currentElement.OnlyBorder = true;
                 wfe.analog.update();
             }
-            wfe.analog.update(wfe.analog.currentElementName != undefined ? wfe.analog.currentElementName :'hours');
+            wfe.analog.update(wfe.analog.currentElementName != undefined ? wfe.analog.currentElementName : 'hours');
         },
         update: function (arrow) {
             if (arrow != undefined) {
